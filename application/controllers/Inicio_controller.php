11e5ad4b5188ec3b20c4e0396e20189fab2d2d17
bryane9911061563 <bryanedilberto@hotmail.com>
2020-09-09 09:19:35 -0500

    Agregado de archivos iniciales del proyecto

---
+++ application/controllers/Inicio_controller.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+class Inicio_controller extends CI_Controller
+{
+    
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  public function index()
+  {
+    $this->load->view('masterpage/Head_view');
+    $this->load->view('masterpage/Navbar_view');
+    $this->load->view('masterpage/Sidebar_view');
+    $this->load->view('publico/Index_view');
+    $this->load->view('masterpage/Footer_view');
+  }
+
+}
+
+
+/* End of file Inicio_controller.php */
+/* Location: ./application/controllers/Inicio_controller.php */
