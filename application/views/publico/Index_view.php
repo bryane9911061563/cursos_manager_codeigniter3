5ae51248d150eb16b5e0d03cfdaddd2fc376e49b
bryane9911061563 <bryanedilberto@hotmail.com>
2020-09-11 01:03:31 -0500

    Se modifico titulo de la pagina principal index

--- application/views/publico/Index_view.php
+++ application/views/publico/Index_view.php
@@ -6,7 +6,7 @@
         <div class="card mb-4">
             <div class="card-body">
                 <p class="mb-0">
-                    This page is an example of using static navigation. By removing the
+                    Esta es una prueba de pagina estatica. By removing the
                     <code>.sb-nav-fixed</code>
                     class from the
                     <code>body</code>
